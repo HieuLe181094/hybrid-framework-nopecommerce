dfd269a91cbd432068d0ed1f9216b7ba60532421
Hieu Le <lengoduchieu@gmail.com>
2025-05-21 08:08:40 +0700

    Add Page Object in pageFactory

--- actions/pageFactory/nopCommerce/CustomerInfoPageObject.java
+++ actions/pageFactory/nopCommerce/CustomerInfoPageObject.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import pageUIs.nopCommerce.CustommerPageUI;
 
 public class CustomerInfoPageObject extends BagePage {
     private WebDriver driver;
