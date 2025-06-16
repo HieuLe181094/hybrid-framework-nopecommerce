fdce82a52afd0e3caae69a29187f5f6dcfbfff7b
Hieu Le <lengoduchieu@gmail.com>
2025-06-16 20:41:13 +0700

    Fix: remove hardcoded token from GlobalConstants.java

--- actions/commons/GlobalConstants_notPublic.java
+++ actions/commons/GlobalConstants_notPublic.java
@@ -1,5 +1,6 @@
 package commons;
 
 public class GlobalConstants_notPublic {
-    public static final String JIRA_API_KEY = "ATATT3xFfGF0VTEgtaA7aQSop1WQ6WpTyGd_0tyUDPobNuGuR8AtBV1TN-oo2SvYxcLKxV2hDeFeOayU9vVDV87xztBEN0U5P6xnvB2UILP2z1nF3laMNvQjpL-gKyaYmMzepd9tcNHl-7y1sbM--Od4tWBiH2B6GHssIqf7kho7B9amIuaP5uw=85727426";
+    public static final String ATLASSIAN_API_TOKEN = System.getenv("ATLASSIAN_API_TOKEN");
+
 }
