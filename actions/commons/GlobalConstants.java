9679e962cddbfd336a08a380493ae71f9348ba3e
Hieu Le <lengoduchieu@gmail.com>
2025-06-16 20:38:39 +0700

    Fix: remove hardcoded token from GlobalConstants.java

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -44,7 +44,7 @@ public class GlobalConstants {
 
     public static final String JIRA_SITE_URL = "https://lengoduchieu-auto.atlassian.net/";
     public static final String JIRA_USERNAME = "lengoduchieu@gmail.com";
-    public static final String JIRA_API_KEY = "ATATT3xFfGF0VTEgtaA7aQSop1WQ6WpTyGd_0tyUDPobNuGuR8AtBV1TN-oo2SvYxcLKxV2hDeFeOayU9vVDV87xztBEN0U5P6xnvB2UILP2z1nF3laMNvQjpL-gKyaYmMzepd9tcNHl-7y1sbM--Od4tWBiH2B6GHssIqf7kho7B9amIuaP5uw=85727426";
+
     public static final String JIRA_PROJECT_KEY ="SCRUM";
 
 }
